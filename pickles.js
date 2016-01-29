8387ead9abeee4030467136602bcc215d15e8791
Anders Bakken <agbakken@gmail.com>
2016-01-29 13:25:42 -0800

    Bypass homework stuff

--- pickles.js
+++ pickles.js
@@ -4,6 +4,13 @@
 'use strict';
 
 console.log("STARTING");
-var zwave = require('./zwave');
-zwave.start("/dev/ttyACM0");
-console.log("STARTED");
+
+var openzwave = new require('openzwave-shared');
+openzwave.connect("/dev/ttyACM0");
+openzwave.on("node event", function(nodeId, value) {
+    console.log("GOT NODE EVENT", value);
+});
+
+// var zwave = require('./zwave');
+// zwave.start("/dev/ttyACM0");
+// console.log("STARTED");
